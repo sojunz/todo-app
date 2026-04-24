b8897c844693318d1496a03b995b384a1564497b
SoHyung Chang <sinchonblues@gmail.com>
2026-04-24 12:55:20 +1200

    Fix TodoForm import path for Vite

--- src/TodoPage.jsx
+++ src/TodoPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import TodoForm from './TodoForm';
+import TodoForm from "./TodoForm.jsx";
 import TodoItem from "./TodoItem";
 import "./Todo.css";
 
